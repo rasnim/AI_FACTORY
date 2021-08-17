96b69506b0e0cc21a2e7de9038266e86abce3ddb
rasnim <bjh713@khu.ac.kr>
2021-08-17 14:45:44 +0900

    Initial commit

--- (Image Classification)Surface_crack/app.py
+++ (Image Classification)Surface_crack/app.py
@@ -20,12 +20,13 @@ if __name__ == '__main__':
     val_datagen = df_augment(prep_func, rot_range=0, zoom_range=0, width_shift=0, height_shift=0,
                              hor_flip=False, ver_flip=False)
 
+    # dataset
     train_bs, train_generator = dataset_generator(batch_size=16, dataset_datagen=train_datagen, dataset_df=train_df,
                                                   cls_mod='binary', shuf=True)
     val_bs, val_generator = dataset_generator(batch_size=16, dataset_datagen=val_datagen, dataset_df=val_df,
                                               cls_mod='binary', shuf=False)
-    test_generator = dataset_generator(batch_size=16, dataset_datagen=val_datagen, dataset_df=test_df,
-                                       cls_mod='binary', shuf=False)
+    test_bs, test_generator = dataset_generator(batch_size=16, dataset_datagen=val_datagen, dataset_df=test_df,
+                                                cls_mod='binary', shuf=False)
 
     # model
     clf_model = model_structure(vgg_model)
